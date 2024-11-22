fe655fa1c0fb5b297d92db8860de58642213a652
AbhinavVerma2000 <75626837+AbhinavVerma2000@users.noreply.github.com>
2024-11-22 15:00:30 +0530

    Update StringReverse.java

--- StringReverse.java
+++ StringReverse.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import java.io.*;
 import java.util.*;
 
@@ -12,18 +11,3 @@ public class Solution {
         System.out.println( A.equals( new StringBuilder(A).reverse().toString()) ? "Yes" : "No" );
     }
 }
-=======
-import java.io.*;
-import java.util.*;
-
-public class Solution {
-
-    public static void main(String[] args) {
-        
-        Scanner sc=new Scanner(System.in);
-        String A=sc.next();
-        /* Enter your code here. Print output to STDOUT. */
-        System.out.println( A.equals( new StringBuilder(A).reverse().toString()) ? "Yes" : "No" );
-    }
-}
->>>>>>> 9df9189a5cb3859214376f80b8e9e77f30c19160
