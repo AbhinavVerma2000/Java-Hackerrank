6ed70aaa5e19eb35d7661347be989760a2913da9
AbhinavVerma2000 <75626837+AbhinavVerma2000@users.noreply.github.com>
2024-11-22 14:53:15 +0530

    Update CurrencyFormatter.java

--- CurrencyFormatter.java
+++ CurrencyFormatter.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import java.util.*;
 import java.text.*;
 
@@ -21,3 +22,28 @@ public class Solution {
         
     }
 }
+=======
+import java.util.*;
+import java.text.*;
+
+public class Solution {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+        
+        Locale indiaLocale = new Locale("en", "IN");
+        NumberFormat us     = NumberFormat.getCurrencyInstance(Locale.US);
+        NumberFormat india  = NumberFormat.getCurrencyInstance(indiaLocale);
+        NumberFormat china  = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        NumberFormat france = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+        System.out.println("US: "     + us.format(payment));
+        System.out.println("India: "  + india.format(payment));
+        System.out.println("China: "  + china.format(payment));
+        System.out.println("France: " + france.format(payment));
+        
+        
+    }
+}
+>>>>>>> 9df9189a5cb3859214376f80b8e9e77f30c19160
